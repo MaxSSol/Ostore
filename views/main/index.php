b6ca11c77e2a4db10114023829a1e51e8fc7fbb7
MaxSSol <49018067+MaxSSol@users.noreply.github.com>
2021-04-28 13:53:25 +0300

    Add main direcory and add index.php file

---
+++ views/main/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Ostore</title>
+ </head>
+ <body>
+  <h1>The Main page</h1>
+ </body>
+</html>

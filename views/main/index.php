3498c50d9f10d4e6e97b650d63ca3f48033e5f61
MaxSSol <49018067+MaxSSol@users.noreply.github.com>
2021-04-29 18:27:24 +0300

    Add home page design in index.php file

--- views/main/index.php
+++ views/main/index.php
@@ -1,10 +1,89 @@
 <!DOCTYPE html>
-<html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Ostore</title>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Ostore</title>
+    <link rel="stylesheet" href="../../public/style/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
  </head>
- <body>
-  <h1>The Main page</h1>
- </body>
+<body>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-5 border-bottom">
+            <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">Ostore</a>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> Contacts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cart</a>
+                </li>
+            </ul>
+            <button type="button" class="btn btn-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+    </header>
+    <section class="promo d-flex justify-content-center align-items-center mb-5">
+        <div class="promo-inner">
+            <p class="promo-title m-0">Slider</p>
+        </div>
+    </section>
+    <section class="top-products mb-5">
+        <p class="top-products-title d-flex justify-content-center mb-5">Only in Ostore</p>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+            <div class="col">
+                <div class="card">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Product</text></svg>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Product1</p>
+                    <p class="card-text">Description</p>
+                    <div class="card-btn d-flex justify-content-center">
+                        <button type="button" class="btn btn-primary me-2">View</button>
+                        <button type="button" class="btn btn-primary">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Product</text></svg>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Product1</p>
+                    <p class="card-text">Description</p>
+                    <div class="card-btn d-flex justify-content-center">
+                        <button type="button" class="btn btn-primary me-2">View</button>
+                        <button type="button" class="btn btn-primary">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Product</text></svg>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Product1</p>
+                    <p class="card-text">Description</p>
+                    <div class="card-btn d-flex justify-content-center">
+                        <button type="button" class="btn btn-primary me-2">View</button>
+                        <button type="button" class="btn btn-primary">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="footer d-flex justify-content-center align-items-center">
+        <div class="footer-inner">
+                <p class="footer-text m-0">FOOTER</p>
+        </div>
+    </footer>
+</div>
+</body>
 </html>

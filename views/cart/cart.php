4657f2b96493209db1c73f83f980d8cd177fb23f
MaxSSol <49018067+MaxSSol@users.noreply.github.com>
2021-04-29 20:28:15 +0300

    Add cart.php cart.css and update product.php file

--- views/cart/cart.php
+++ views/cart/cart.php
@@ -36,34 +36,16 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
+
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Tools</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Name Brand</a>
+                    <a class="nav-link" href="#">Cart</a>
                 </li>
             </ul>
         </div>
     </section>
-    <section class="product mb-5">
-        <div class="product-name">
-            <p class="h3">Tools</p>
-        </div>
-        <div class="product-inner d-flex justify-content-between align-items-center">
-            <div class="product-img">
-                <img src="https://www.kstools.com/media/image/10/79/56/FOT_GES_ALG_917-0797-GB_SALL_AING_V15c91d4e3dd32e_600x600.jpg" alt="tools"/>
-            </div>
-            <div class="product-info">
-                <p>Price</p>
-                <p>Description</p>
-                <button type="button" class="btn btn-primary me-2">Checkout</button>
-                <button type="button" class="btn btn-primary">Add to Cart</button>
-            </div>
-        </div>
-    </section>
-    <section class="description">
-        <div class="description-inner d-flex justify-content-center align-items-center">
-            <p class="description-title m-0">Description</p>
+    <section class="cart">
+        <div class="cart-inner d-flex justify-content-center align-items-center">
+            <p class="h3 m-0">Cart</p>
         </div>
     </section>
     <footer class="footer d-flex justify-content-center align-items-center">

591d6c1569c43176365c702889082cb77c5c5346
MaxSSol <49018067+MaxSSol@users.noreply.github.com>
2021-05-09 14:33:34 +0300

    Add View class

--- src/core/View.php
+++ src/core/View.php
@@ -1,7 +1,29 @@
 <?php
-
-
+namespace src\core;
 class View
 {
+    protected $route;
+    public function __construct($route)
+    {
+        $this->route = $route;
 
+    }
+    public function render(string $title,array $params = [],string $template,string $layout = 'default')
+    {
+        if(isset($params))
+        {
+            extract($params);
+        }
+
+        //$path = __DIR__.'/../views/'.$this->route.'.php'; include route
+        $path = __DIR__.'/../views/'.$template.'.php';//$template = 'main/index.php' only!!!
+        if(file_exists($path))
+        {
+            ob_start();
+            require_once  $path;
+            $content = ob_get_clean();
+            require_once __DIR__.'/../views/layouts/'.$layout.'.php';
+        }
+
+    }
 }

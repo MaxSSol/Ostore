95f5aa3427faced7eb7c2e3e5684304730c294c8
MaxSSol <49018067+MaxSSol@users.noreply.github.com>
2021-05-11 20:38:22 +0300

    Add error404.php file

--- Framework/Core/View.php
+++ Framework/Core/View.php
@@ -17,18 +17,19 @@ class View
             }
 
             //$path = __DIR__.'/../View/'.$this->route.'.php'; include route
-            $path = __DIR__ . '/../../src/View/' . $template . '.php';//$template = 'main/index.php' only!!!
+            $path = __DIR__ . '/../../src/View/'.$template.'.php';//$template = 'main/index.php' only!!!
             if (file_exists($path)) {
                 ob_start();
                 require_once $path;
                 $content = ob_get_clean();
-                require_once __DIR__ . '/../../src/View/layouts/' . $layout . '.php';
+                require_once __DIR__ . '/../../src/View/layouts/'.$layout.'.php';
             } else {
                 throw new ViewException();
             }
         }catch (ViewException $e)
         {
-            $this->render('Ostore',['code'=>'404','message'=>$e->getMessage()],'Error/error.php');
+            $errorView = new View('error/error404.php');
+           $errorView->render('Error',['code'=>$e->getCode(),'message'=>$e->getMessage()],'error/error404','error');
         }
     }
 }
---
+++ src/View/error/error404.php
@@ -0,0 +1,2 @@
+<h1 class="d-flex justify-content-center m-0 mt-5"><?php echo '#'.$code ?? '';?></h1>
+<p class="d-flex justify-content-center m-0"><?php echo $message ?? '';?></p>

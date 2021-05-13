2c3b16811fa22a95b88f6f9b8eef50a597b4a62f
Maksym Solomianyi <maxssolom@gmail.com>
2021-05-13 11:31:51 +0300

    Update View.php for work with the Router

--- Framework/Core/View.php
+++ Framework/Core/View.php
@@ -1,15 +1,19 @@
 <?php
 namespace Framework\Core;
+
 use Framework\Exception\ViewException;
+
 class View
 {
     protected $route;
+
     public function __construct($route)
     {
         $this->route = $route;
 
     }
-    public function render(string $title,array $params = [],string $template,string $layout = 'default')
+
+    public function render(string $title, array $params = [], string $layout = 'default')
     {
         try {
             if (isset($params)) {
@@ -17,19 +21,20 @@ class View
             }
 
             //$path = __DIR__.'/../View/'.$this->route.'.php'; include route
-            $path = __DIR__ . '/../../src/View/'.$template.'.php';//$template = 'main/index.php' only!!!
+            $path = __DIR__ . '/../../src/View/' . $this->route['controller'] . '/' . $this->route['action'] . '.php';
             if (file_exists($path)) {
                 ob_start();
                 require_once $path;
                 $content = ob_get_clean();
-                require_once __DIR__ . '/../../src/View/layouts/'.$layout.'.php';
+                require_once __DIR__ . '/../../src/View/layouts/' . $layout . '.php';
             } else {
                 throw new ViewException();
             }
-        }catch (ViewException $e)
-        {
-            $errorView = new View('error/error404.php');
-           $errorView->render('Error',['code'=>$e->getCode(),'message'=>$e->getMessage()],'error/error404','error');
+        } catch (ViewException $e) {
+            $route = ['controller' => 'error', 'action' => 'error404'];
+            $errorView = new View($route);
+            $errorView->render('Error', ['code' => $e->getCode(), 'message' => $e->getMessage()], 'error');
         }
     }
 }
+

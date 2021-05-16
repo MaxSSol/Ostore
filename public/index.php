538bdfa4ed8270b6dd7c7e720ab4c1fe0d9aea59
MaxSSol <49018067+MaxSSol@users.noreply.github.com>
2021-05-16 13:04:31 +0300

    Update autoloader.php file

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 use src\autoloader\Autoloader;
 use src\core\View;
 require_once __DIR__.'/../src/autoloader/Autoloader.php';
-$obj = new Autoloader();
-$obj->load('src\\core\\View');
+$autoloader = new Autoloader();
+spl_autoload_register([$autoloader,'load'],false,true);
 $view = new View('main/index.php');
 $view->render('Home',['css'=>'style/main.css'],'main/index');
+
--- src/autoloader/Autoloader.php
+++ src/autoloader/Autoloader.php
@@ -1,17 +1,21 @@
 <?php
 namespace src\autoloader;
-
+use Exception;
 class Autoloader
 {
     public function load($className)
     {
-        spl_autoload_register(function ($className){
-            $parts = explode('\\',$className);
-            $path = __DIR__.'/../../'.implode('/',$parts).'.php';
-            if(file_exists($path)){
+        try {
+            var_dump($className);
+            $parts = explode('\\', $className);
+            $path = __DIR__ . '/../../' . implode('/', $parts) . '.php';
+            if (file_exists($path)) {
                 require_once $path;
+            } else {
+                throw new Exception('Class not found');
             }
-        });
+        }catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
-
 }

a1a5dd6c76c61681ba05563293f20470374f753b
Maksym Solomianyi <maxssolom@gmail.com>
2021-06-06 11:10:58 +0300

    Update Router class

--- Framework/Router/Router.php
+++ Framework/Router/Router.php
@@ -1,72 +1,89 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Framework\Router;
+
+use Exception;
 
 class Router
 {
-    protected array $routes = [];
-    protected string $params;
-    protected string $controllerName;
-    protected string $actionName;
-    protected array $getParameters = [];
+    private array $routes;
+    private function getMethod(): string
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+    private function isGet(): bool
+    {
+        return $this->getMethod() === 'GET';
+    }
+    private function isPost(): bool
+    {
+        return $this->getMethod() === 'POST';
+    }
+    private function getUrl(): string
+    {
+        $url = $_SERVER['REQUEST_URI'];
+        $param = strpos($url, '?');
+        return is_int($param) ? substr($url, 0, $param) : $url;
+    }
+    public function get(string $path, $handler): void
+    {
+        $path = '#^' . $path . '$#';
+        $this->routes['GET'][$path] = $handler;
+    }
+    public function post(string $path, $handler): void
+    {
+        $path = '#^' . $path . '$#';
+        $this->routes['POST'][$path] = $handler;
+    }
+    public function getBody(): array
+    {
+        $body = [];
+        if ($this->isGet()) {
+            $url = parse_url($_SERVER['REQUEST_URI']);
+            if (isset($url['query'])) {
+                parse_str($url['query'], $body);
+            }
+            return $body;
+        }
+        if ($this->isPost()) {
+            foreach ($_POST as $key => $value) {
+                $body[$key] = $value;
+            }
+            return $body;
+        }
+        return $body;
+    }
 
-    public function __construct()
+    /**
+     * @throws Exception
+     */
+    public function checkHandler(): array
     {
-        $arr = require_once __DIR__ . '/../../src/config/routes.php';
-        foreach ($arr as $route => $param) {
-            $this->addRoute($route, $param);
-        }
-    }
-    public function addRoute($route, $params)
-    {
-        $this->routes[$route] = $params;
-    }
-    public function checkMatch(): bool
-    {
-        $url = trim($_SERVER['REQUEST_URI']);
-        foreach ($this->routes as $route => $param) {
-            if (preg_match('#^' . $route . '$#', $url, $matches)) {
-                if (count($matches) == 1) {
-                    $this->internalUrl($route, $param, $url);
-                    return true;
-                } else {
-                    $this->internalUrlWithParams($route, $param, $url);
-                    return true;
-                }
+        $url = $this->getUrl();
+        $method = $this->getMethod();
+        $routes = $this->routes[$method];
+        foreach ($routes as $pattern => $handler) {
+            if (preg_match($pattern, $url, $matches)) {
+                return $handler;
             }
         }
-        return false;
+        throw new Exception('Page not found');
     }
-    public function internalUrl($route, $param, $url): void
+
+    /**
+     * @return false|mixed
+     */
+    public function run(): ?callable
     {
-        $this->params = $param;
-        $itemUrl = array_filter(explode('/', $param));
-        $this->controllerName = ucfirst(array_shift($itemUrl)) . 'Controller';
-        $this->actionName = array_shift($itemUrl) . 'Action';
-    }
-    public function internalUrlWithParams($route, $param, $url): void
-    {
-        $internalUrl = preg_replace('#^' . $route . '$#', $param, $url);
-        $itemUrl = array_filter(explode('/', $internalUrl));
-        $this->params = $itemUrl[1] . '/' . $itemUrl[2];
-        $this->controllerName = ucfirst(array_shift($itemUrl)) . 'Controller';
-        $this->actionName = array_shift($itemUrl) . 'Action';
-        $parameters = $itemUrl;
-        $this->getParameters = $itemUrl;
-    }
-    public function run(): bool
-    {
-        if ($this->checkMatch() !== false) {
-            $path = 'src\Controller\\' . $this->controllerName;
-            if (class_exists($path) && method_exists($path, $this->actionName)) {
-                $action = $this->actionName;
-                $controller = new $path($this->params, $this->getParameters);
-                $controller->$action();
-                return true;
-            }
-        } else {
-            echo '<h1 style="text-align: center">Bad request, url not found!</h1>';
-            return false;
+        try {
+            $handler = $this->checkHandler();
+            $handler[0] = new $handler[0]($this->getBody());
+            return call_user_func($handler);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return null;
         }
     }
 }

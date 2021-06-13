8dcfdd578674cf4aa788ecde629f65fe0d7af553
Maksym Solomianyi <maxssolom@gmail.com>
2021-06-13 19:08:05 +0300

    Update ProductController class

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,24 +18,46 @@ class ProductController
         $this->params = $params;
         $this->view = new View();
     }
+    public function getProducts(): void
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        $products = $this->productMapper->getProductList();
+        $json = [];
+        foreach ($products as $product) {
+            $json[] = $product;
+        }
+        echo json_encode($json);
+    }
+    public function getProduct()
+    {
+        if ($this->checkParams() !== false) {
+            header("Access-Control-Allow-Origin: *");
+            header("Content-Type: application/json; charset=UTF-8");
+            $id = (int)$this->params['id'];
+            $product = $this->productMapper->getProductById($id);
+            $jsonProduct = [];
+            foreach ($product as $key => $param) {
+                $jsonProduct[$key] = $param;
+            }
+            echo json_encode($jsonProduct);
+        }
+    }
     public function viewAction(): void
     {
-        $products = $this->productMapper->getProductList();
-        $this->view->render('show/show', 'Products', ['css' => 'style/showProducts.css', 'products' => $products]);
+        $this->view->render(
+            'show/show',
+            'Products',
+            ['css' => 'style/showProducts.css','js' => 'js/products.js']
+        );
     }
     public function viewProductAction(): void
     {
-        if ($this->checkParams()) {
-            $id = (int)$this->params['id'];
-            $product = $this->productMapper->getProductById($id);
-            $this->view->render(
-                'show/product',
-                $product->getTitle(),
-                ['css' => 'style/product.css', 'product' => $product]
-            );
-        } else {
-            echo 'Product not found';
-        }
+        $this->view->render(
+            'show/product',
+            'Product',
+            ['css' => 'style/product.css', 'js' => 'js/product.js']
+        );
     }
     private function checkParams(): bool
     {

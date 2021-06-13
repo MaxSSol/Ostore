e34d7cda82d20e3194e702584d084f101efcb568
Maksym Solomianyi <maxssolom@gmail.com>
2021-06-13 19:07:34 +0300

    Update template file

--- src/View/show/product.php
+++ src/View/show/product.php
@@ -1,14 +1,14 @@
-<section class="product mb-5">
+<section class="product mb-5" id="product">
     <div class="product-name">
-        <p class="h3"><?= $product->getTitle();?></p>
+        <p class="h3"></p>
     </div>
     <div class="product-inner d-flex justify-content-between align-items-center">
         <div class="product-img">
             <img src="https://www.kstools.com/media/image/10/79/56/FOT_GES_ALG_917-0797-GB_SALL_AING_V15c91d4e3dd32e_600x600.jpg" alt="tools"/>
         </div>
         <div class="product-info">
-            <p class="product-price" align="center">Price: $<?= $product->getPrice();?></p>
-            <p class="product-amount" align="center">In stock: <?= $product->getAmount();?></p>
+            <p class="product-price" align="center">Price: $</p>
+            <p class="product-amount" align="center">In stock:</p>
             <a class="btn btn-primary me-2" href="/order">Checkout</a>
             <a class="btn btn-primary" href="/cart/add">Add to Cart</a>
         </div>
@@ -17,7 +17,7 @@
 <section class="description">
     <div class="description-inner">
         <p class="fs-3 title">Description</p>
-        <p class="description-title" align="justify"><?= $product->getDescription();?></p>
+        <p class="description-title" align="justify"></p>
     </div>
 </section>
 
--- src/View/show/show.php
+++ src/View/show/show.php
@@ -1,20 +1,10 @@
-<section class="products mb-5">
-    <div class="row">
-        <?php foreach ($products as $product): ?>
-        <div class="col-3">
-            <div class="card">
-                <img width="200px" height="200px" src="https://inlnk.ru/WRppd"/>
-                <div class="card-body">
-                    <p class="card-text"><?= $product->getTitle();?></p>
-                    <p class="card-text">Price: $<?= $product->getPrice();?></p>
-                    <div class="card-btn d-flex justify-content-center align-items-center">
-                        <a class="btn btn-primary me-2" href="/product?id=<?= $product->getId();?>">View product</a>
-                        <a class="btn btn-primary px-4" href="/card/add?id=<?= $product->getId();?>">Add to cart</a>
-                    </div>
-                </div>
+<section>
+    <div class="products mb-5">
+        <div class="row">
+            <?php for ($i = 0;$i < 50;$i++):?>
+            <div class="col-3" id="product<?php echo $i;?>">
             </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endfor;?>
     </div>
     <div class="products-button d-flex justify-content-center">
         <button type="button" class="btn btn-primary me-2">1</button>

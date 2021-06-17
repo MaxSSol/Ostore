a86bea635acb88f0363757d26faaf42ff93f3669
Maksym Solomianyi <maxssolom@gmail.com>
2021-06-17 22:39:51 +0300

    Update product template

--- src/View/show/product.php
+++ src/View/show/product.php
@@ -4,13 +4,13 @@
     </div>
     <div class="product-inner d-flex justify-content-between align-items-center">
         <div class="product-img">
-            <img src="https://www.kstools.com/media/image/10/79/56/FOT_GES_ALG_917-0797-GB_SALL_AING_V15c91d4e3dd32e_600x600.jpg" alt="tools"/>
+            <img src="<?php echo $product->getProductPhoto();?>" alt="<?= $product->getTitle();?>"/>
         </div>
         <div class="product-info">
             <p class="product-price" align="center">Price: $<?= $product->getPrice();?></p>
             <p class="product-amount" align="center">In stock: <?= $product->getAmount();?></p>
             <a class="btn btn-primary me-2" href="/order">Checkout</a>
-            <a class="btn btn-primary" href="/cart/add">Add to Cart</a>
+            <a class="btn btn-primary" href="/cart/add?id=<?= $product->getId();?>">Add to Cart</a>
         </div>
     </div>
 </section>

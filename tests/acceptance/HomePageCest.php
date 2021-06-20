3b2b992b10d7a0920e1da1568c4420bd9e213575
Maksym Solomianyi <maxssolom@gmail.com>
2021-06-20 23:06:16 +0300

    Add tests directory with acceptance and unit tests

---
+++ tests/acceptance/HomePageCest.php
@@ -0,0 +1,17 @@
+<?php
+
+class HomePageCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTestHomePageSuccess(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Products');
+        $I->see('Only in Ostore');
+        $I->see('Delivery');
+    }
+}
---
+++ tests/unit/OrderStatusMapperCest.php
@@ -0,0 +1,68 @@
+<?php
+
+use Framework\DataMapper\OrderStatusMapper;
+use src\Model\OrderStatus;
+
+class OrderStatusMapperCest
+{
+    private OrderStatus $order;
+    private OrderStatusMapper $orderStatusMapper;
+    private int $lastInsertId;
+    public function _before(UnitTester $I)
+    {
+        $this->order = new OrderStatus();
+        $this->orderStatusMapper = new OrderStatusMapper();
+    }
+
+    // tests
+    public function tryToTestAddOrder(UnitTester $I)
+    {
+        $order = $this->order;
+        $order->setProductId(22);
+        $order->setUserId(2);
+        $order->setProductQuantity(4);
+        $order->setStatus('Processed');
+        $order->setComment('empty');
+        ($this->orderStatusMapper)->addOrder($order);
+        $this->lastInsertId = $this->orderStatusMapper->getLastInsertId();
+        $I->seeInDatabase(
+            'order_status',
+            [
+                'product_id' => 22,
+                'user_id' => 2,
+                'quantity' => 4,
+                'status' => 'Processed',
+                'comment' => 'empty'
+            ]
+        );
+    }
+    public function tryToTestUpdateOrder(UnitTester $I)
+    {
+        $order = $this->order;
+        $order->setId($this->lastInsertId);
+        $order->setProductId(22);
+        $order->setUserId(2);
+        $order->setProductQuantity(5);
+        $order->setStatus('Processed');
+        $order->setComment('empty');
+        ($this->orderStatusMapper)->updateOrder($order);
+        $I->seeInDatabase(
+            'order_status',
+            [
+                'id' => $this->lastInsertId,
+                'product_id' => 22,
+                'user_id' => 2,
+                'quantity' => 5,
+                'status' => 'Processed',
+                'comment' => 'empty'
+            ]
+        );
+    }
+    public function tryToTestDeleteOrder(UnitTester $I)
+    {
+        $order = $this->order;
+        $order->setId($this->lastInsertId);
+        ($this->orderStatusMapper)->deleteOrder($order);
+        $I->dontSeeInDatabase('order_status', ['id' => $this->lastInsertId]);
+    }
+}
